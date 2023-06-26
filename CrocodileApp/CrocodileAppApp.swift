5514e12a415cefa2a4c78e65e98437c2ec1062dc
Aleksey <oldf03@gmail.com>
2023-06-26 12:17:21 +0300

    Initial Commit

---
+++ CrocodileApp/CrocodileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrocodileAppApp.swift
+//  CrocodileApp
+//
+//  Created by Buba on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CrocodileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
